45259a87978d5010e07665a175f07393ac406d83
+ <+>
2022-08-22 22:45:16 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("After dom has loaded");
+    document.getElementById('text').innerHTML = "This is really cool!";
+});
